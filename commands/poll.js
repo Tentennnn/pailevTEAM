e60e3fe1128dc39d50343f15ce8a1bb3f37405ce
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-19 20:56:44 +0700

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -2,6 +2,9 @@
 const Discord = require("discord.js");
 
 exports.run = (client, message, Discord, prefix) => {
+
+const args = message.content.slice(prefix.length).split(/ +/);
+const commandName = args.shift().toLowerCase(); 
     message.channel.send("@everyone");
     message.delete(900);
     let poll = args.join(" ");
