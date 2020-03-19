d583b63d7fe3a1dc00fce73e482f4ab4ec5fa314
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-19 20:54:07 +0700

    Create poll.js

---
+++ commands/poll.js
@@ -0,0 +1,20 @@
+
+const Discord = require("discord.js");
+
+exports.run = (client, message, Discord, prefix) => {
+    message.channel.send("@everyone");
+    message.delete(900);
+    let poll = args.join(" ");
+
+    
+
+    let announceEmbed = new Discord.RichEmbed()
+    .setColor(`RANDOM`)
+    .setTimestamp()
+    .setTitle(poll)
+
+    message.channel.send(announceEmbed);
+    message.react(`✅`);
+    message.react(`❌`);
+
+}
