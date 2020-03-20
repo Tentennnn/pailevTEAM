696fc6a750a4e8899377f60d44efff17b2688a09
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-20 16:45:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -147,18 +147,21 @@ client.on("message", async message => {
 }
 
   if(command === "picture") {
-        message.delete(1000);
-    if(message.author.id !== "356510829920780289") return;
-     let args = args.join(" ");
-     let sembad = new Discrd.RichEmbed()
-        .setColor('RANDOM')
-        .setImage('args')
-        .setTitle('PICTURE RATEING :')
-        
-        message.channel.send(sembad);
-        message.react('👍');
-        message.react('👎');
- }
+ message.delete()
+if(args[0] == "help"){
+  message.channel.send("**Enter • <YTUsername> <VideoURL>**").then(msg => msg.delete(11000));
+    return;
+  }
+  let txt = args[0]; 
+  let pictureURL = args[1];
+ 
+    send(message.channel, {//`📌 @here\n\nUsername ● **${txt}** Has Upload Video From YouTube !\n\n${pictureURL} __**Go Check Now**__`, {
+        name: 'PICTURE :',
+        icon: 'https://discordapp.com/channels/430630483408453633/690183762633424927/690493280953303040',
+        image: {
+		        url: '${pictureURL}'
+    })
+}
 
 });
 
