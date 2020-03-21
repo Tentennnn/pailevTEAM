b03d808bfbc71a0c40738d0ee7e233339162f2ab
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-21 12:01:12 +0700

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -3,6 +3,8 @@ const Discord = require('discord.js'),
     table = require('table');
 
 exports.run = async (client, message, args, tools) => {
+       const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+       const command = args.shift().toLowerCase(); 
 
   if(args.join(' ').length > 14) return message.channel.send('Only 14 characters are admitted!') 
   if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000})); 
