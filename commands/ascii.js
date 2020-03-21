ace9f772370a40083fdb9580737474920acfbb7b
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-21 12:13:36 +0700

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -31,6 +31,8 @@ exports.run = async (bot, message, args, tools) => {
    
        const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
        const command = args.shift().toLowerCase(); 
+       var args = message.content.split(' ').slice(1);
+
   if(args.join(' ').length > 14) return message.channel.send('Only 14 characters are admitted!') 
   if (!args.join(' ')) return message.channel.send('Please, provide text to format in ASCII! Usage: ascii <text>').then(msg => msg.delete({timeout: 10000})); 
     figlet(args.join(' '), (err, data) => {
