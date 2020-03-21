a26ff8c8b9ffa678d14ba55356e78f8713c32248
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-21 10:47:07 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,16 +6,23 @@ exports.run = (client, message, Discord, prefix) => {
 const args = message.content.slice(prefix.length).split(/ +/);
 const commandName = args.shift().toLowerCase(); 
  
- //message.delete(100);
+ message.delete(9000);
     //if(message.author.id !== "356510829920780289") return;
     
    // let say = args.join("");
     let embedsay = new Discord.RichEmbed()
        .setColor('RANDOM')
-       .setDescription('THIS BOT WILL HOST SOON....\n\n\n**CONTACT :**\n\nDEVOLEPOR : Ort Brap Tuk Oy Chngol 😎 \nFACEBOOK : [CLICK HERE](https://www.facebook.com/heaalong855)\nDISCORD : [CLICK HERE](https://discord.gg/ZWWD7zT)')
-       .setTitle(message.guild.name)
+       //.setDescription(``)
+       .setTitle(`${message.guild.name} | ${prefix}help`)
+    .addField("Dev", "`leftserver`  `glist`")
+    .addField("Moderation", "`clear`  `say`  `kick`  `ban`")
+    .addField("Info", "`serverinfo`   `topinvites`  `dev`  `invitelist`  `banslist`")
+    .addField("General", "`ping`  `avatar`  `emojilist`   `invite`  `botstats`   `userinfo`  ")
+    .addField("Fun", "`get`  `ascii`  `gif`  `random` `status`  `luckymunber`  `magik` ")
+    .addField("Roleplay", "`pat`  `kiss`  `love`")
        
     message.channel.send(embedsay)
+    message.react("📎")
     //message.delete(100000);
  
  
