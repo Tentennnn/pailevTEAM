bd200c7c271883d361425281d2854ebbf2cec0f6
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-27 17:35:21 +0700

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-module.exports.run = async (client, message, prefix ) => {
+module.exports.run = async (client, message, Discord, prefix) => {
   const args = message.content.slice(prefix.length).split(/ +/);
 
 
