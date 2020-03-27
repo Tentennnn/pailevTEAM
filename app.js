79346fb7f95db38273e4f89b14434dcb46e68881
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-27 16:48:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -44,54 +44,7 @@ function setActivity() {
 
 setInterval(setActivity, 1000 * 60 * 2)
 
- const canvas = require('Canvas');
-const applyText = (canvas, text) => {
-	const ctx = canvas.getContext('2d');
 
-	// Declare a base size of the font
-	let fontSize = 70;
-
-	do {
-		// Assign the font to the context and decrement it so it can be measured again
-		ctx.font = `${fontSize -= 10}px sans-serif`;
-		// Compare pixel width of the text to the canvas minus the approximate avatar size
-	} while (ctx.measureText(text).width > canvas.width - 300);
-
-	// Return the result to use in the actual canvas
-	return ctx.font;
-};
-
-client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find('name', 'welcome');
-	if (!channel) return;
-
-	const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('./wallpaper.png');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	// Assign the decided font to the canvas
-	ctx.font = applyText(canvas, member.displayName);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(member.displayName, canvas.width / 2.5, canvas.height / 1.8);
-
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-
-	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
-
-	channel.send(`Welcome to the server, ${member}!`, attachment);
-});
-   
 client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.startsWith(prefix)){
