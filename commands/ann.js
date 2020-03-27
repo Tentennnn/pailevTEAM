fb0941df219ba78f15f102e4469654abd9ce9a64
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-27 17:33:00 +0700

    Create ann.js

---
+++ commands/ann.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+module.exports.run = async (client, message, prefix ) => {
+  const args = message.content.slice(prefix.length).split(/ +/);
+
+
+message.channel.send("@everyone").then(msg => {msg.delete(3000);
+let message = args.join(" ");
+const embed = new Discord.RichEmbed()
+.setColor("#ccc")
+.setTitle("📢Annoucements")
+.setDescription(message)
+message.channel.send(embed)
