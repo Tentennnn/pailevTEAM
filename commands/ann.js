63be7fc403bdba5d8735cc481faa9c3b09271f5e
longlong5330 <60765259+longlong5330@users.noreply.github.com>
2020-03-27 18:09:30 +0700

    Update ann.js

--- commands/ann.js
+++ commands/ann.js
@@ -3,12 +3,13 @@ const client = new Discord.Client();
 module.exports.run = async (client, message, Discord, prefix) => {
   const args = message.content.slice(prefix.length).split(/ +/);
 
+message.channel.send("@everyone").then(msg => {msg.delete(3000)});
 //let channel = message.channels.find(channel => channel.name === "🎮-announcements");
     let chatchannel = message.guild.channels.find(`name`, "🎮-announcements");
     if(!chatchannel) return message.channel.send("you need create channel #announcements to use annto !");
 
 
-message.channel.send("@everyone").then(msg => {msg.delete(3000)});
+
 let txt = args.join(" ");
 const embed = new Discord.RichEmbed()
 .setColor("#ccc")
